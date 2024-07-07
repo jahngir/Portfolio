e9946275f6ef304386b6af61640959ee1c8f8386
Jahangir Naseer <49036214+jahngir@users.noreply.github.com>
2024-07-08 00:01:10 +0500

    final touch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-      <nav className="mb-20 flex items-center justify-between py-6">
+      <nav className="mb-20 flex items-center justify-between py-2">
         <div className="flex flex-shrink-0 items-center">
           {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
           <span className="text-4xl tracking-tight">JN</span>
